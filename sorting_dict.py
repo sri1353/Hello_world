3382fed4530d6afb02859a7fca64d515bfe09d92
sri1353 <41191425+sri1353@users.noreply.github.com>
2019-12-10 21:40:08 +0530

    For sorting the json file

---
+++ sorting_dict.py
@@ -0,0 +1,29 @@
+import json
+import os
+
+os.chdir("C:/Users/sri-krishna/Desktop")
+
+
+with open('test.json', 'r')as fileobj:
+    json_data = json.load(fileobj)
+
+file_dict = json_data['backends']
+final_dict = {}
+# print(file_dict)
+# print(json.dumps(json_data, indent=4, sort_keys=True))
+
+for index, item in enumerate(file_dict):
+    for key, value in item.items():
+        if key == "properties":
+            for index1, item1 in enumerate(value):
+                for key1, value1 in item1.items():
+                    new_key = str(key1) + '_' + str(index) + '_' + str(index1)
+                    final_dict[new_key] = item1[key1]
+        else:
+            new_key = str(key) + '_' + str(index)
+            final_dict[new_key] = item[key]
+
+
+for key, value in final_dict.items():
+    print(str(key) + ':' + str(value))
+# print(final_dict)
